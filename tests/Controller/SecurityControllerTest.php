56bcb1ebd6262c0b2c8764d4185fbae8a3a4b28f
CamileGhastine <camileghastine@hotmail.com>
2021-01-19 09:07:51 +0100

    Test display form login

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -4,11 +4,15 @@
 namespace App\Tests\Controller;
 
 
+use App\DataFixtures\UserFixtures;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class SecurityControllerTest extends WebTestCase
 {
+    use FixturesTrait;
+
     private $client;
 
     public function setUp()
@@ -20,4 +24,14 @@ class SecurityControllerTest extends WebTestCase
         $this->client->request('GET', '/login');
         static::assertResponseStatusCodeSame(Response::HTTP_OK);
     }
+
+    public function testLoginFormDisplay(){
+        $crawler = $this->client->request('GET', '/login');
+        $this->assertSelectorExists('form');
+        $this->assertSame(2, $crawler->filter('input')->count());
+        $this->assertSame(1, $crawler->filter('label:contains("Nom d\'utilisateur :")')->count());
+        $this->assertSame(1, $crawler->filter('label:contains("Mot de passe :")')->count());
+        $this->assertSelectorTextSame('button', 'Se connecter');
+    }
+
 }
