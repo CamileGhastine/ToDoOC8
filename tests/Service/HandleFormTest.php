fff756c8624d77d9308e5af738d5b1ed9b62159b
CamileGhastine <camileghastine@hotmail.com>
2021-01-22 23:30:25 +0100

    Test handleForm

--- tests/Service/HandleFormTest.php
+++ tests/Service/HandleFormTest.php
@@ -4,15 +4,9 @@
 namespace App\Tests\Service;
 
 
-use App\Controller\TaskController;
 use App\Entity\Task;
-use App\Form\TaskType;
 use App\Service\HandleForm;
-use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Form;
-use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\HttpFoundation\Request;
 
 class HandleFormTest extends TestCase
@@ -21,22 +15,26 @@ class HandleFormTest extends TestCase
     private $em;
     private $request;
 
+    public function setUp() {
+        $this->form = $this->getMockBuilder('Symfony\Component\Form\Form')
+            ->disableOriginalConstructor()
+            ->setMethods(['handleRequest', 'isSubmitted', 'isValid'])
+            ->getMock();
 
+        $this->em = $this->getMockBuilder('Doctrine\ORM\EntityManagerInterface')
+            ->getMock();
 
-    public function testA() {
-        $task = new Task();
-        $formData = [
-            'tit'
-        ];
-        $form = $this->factory->create(TaskType::class, $task);
-        $form->submit()
-
-        dd($form);
+        $this->request = new Request();
     }
-
 
     public function testHandleFormReturnFormWhenFormIsNotSubmitted() {
         $result = $this->handle(new Task(), false, false);
+
+        $this->assertSame($this->form, $result);
+    }
+
+    public function testHandleFormReturnFalseWhenFormIsSubmittedAndNotValid() {
+        $result = $this->handle(new Task(), true, false);
 
         $this->assertSame($this->form, $result);
     }
@@ -54,28 +52,31 @@ class HandleFormTest extends TestCase
     }
 
     private function handle(?Task $task = null, ?bool $submitted = null, ?bool $valid = null){
-        $this->setFormMethods($submitted, $valid);
+        $this->setFormMethods($task, $submitted, $valid);
 
         $handleForm = new HandleForm($this->em);
         return $handleForm->handle($this->request, $this->form, $task);
     }
 
-    private function setFormMethods(?bool $submitted = null, ?bool $valid = null) {
-        $expects = $valid ? 'once' : 'never';
-        $test = 'once';
+    private function setFormMethods(?Task $task, ?bool $submitted = null, ?bool $valid = null) {
+        $submittedTrue = $submitted ? 'once' : 'never';
+        $SubmitedValidTrue = $submitted && $valid ? 'once' : 'never';
+        $SubmitedValidTaskTrue = $submitted && $valid && $task ? 'once' : 'never';
+
+        $this->em->expects($this->$SubmitedValidTaskTrue())->method('persist');
+        $this->em->expects($this->$SubmitedValidTrue())->method('flush');
 
         $this->form
-            ->expects($this->$test())
+            ->expects($this->once())
             ->method('handleRequest')
             ->willReturn($this->form);
         $this->form
-            ->expects($this->$test())
+            ->expects($this->once())
             ->method('isSubmitted')
             ->willReturn($submitted);
         $this->form
-            ->expects($this->$expects())
+            ->expects($this->$submittedTrue())
             ->method('isValid')
             ->willReturn($valid);
     }
-
 }
